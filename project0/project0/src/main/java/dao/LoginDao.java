9b074b9ff1f73537cdf56453bddc71ac0f2fc3d0
Luis Momox <lumomox@gmail.com>
2025-03-05 20:29:00 -0600

    added classes to project0 branch

---
+++ project0/project0/src/main/java/dao/LoginDao.java
@@ -0,0 +1,32 @@
+package dao;
+
+import controller.AccountController;
+import controller.ConnectionController;
+import io.javalin.Javalin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import service.AccountService;
+
+import java.sql.*;
+
+public class LoginDao {
+    //Logger variable
+    public static final Logger logger = LoggerFactory.getLogger(LoginDao.class);
+    Connection conn = ConnectionController.getConnection();
+    AccountDao accountDao = new AccountDao(conn);
+    AccountService accountService = new AccountService(accountDao);
+    AccountController accountController = new AccountController(accountService);
+
+    public LoginDao() throws SQLException { }
+
+    /*
+    Entry point for the application
+    Todo Crear endpoint /auth/login
+    Todo Handle exceptions
+     */
+    public Javalin startApi() {
+        Javalin app = Javalin.create();
+        app.post("/auth/register", accountController::registerNewAccount);
+        return app;
+    }
+}
